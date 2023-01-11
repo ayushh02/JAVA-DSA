b27279b5ca04e9ba35136c44aee6b7d20bdd33e0
Ayush Kumar <72346028+ayushh02@users.noreply.github.com>
2023-01-11 14:07:44 +0530

    Update and rename Running Sum of 1d Array to Running Sum of 1d Array.java

--- leetcode75/Running Sum of 1d Array.java
+++ leetcode75/Running Sum of 1d Array.java
@@ -1,4 +1,4 @@
-Q. Running Sum of 1d Array
+/*Q. Running Sum of 1d Array
    Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
 
    Return the running sum of nums.
@@ -9,7 +9,7 @@ Output: [1,3,6,10]
 Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
 
 
-Code:
+Code:*/
     class Solution {
     public int[] runningSum(int[] nums) {
         int rs[]=new int[nums.length];
