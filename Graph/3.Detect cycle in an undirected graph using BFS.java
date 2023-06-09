68a9482ce06cd43a6a71a75a61772d58676a64d0
Ayush Kumar <72346028+ayushh02@users.noreply.github.com>
2023-06-10 01:31:10 +0530

    Create 3.Detect cycle in an undirected graph using BFS.java

---
+++ Graph/3.Detect cycle in an undirected graph using BFS.java
@@ -0,0 +1,49 @@
+/*Detect cycle in an undirected graph using BFS*/
+ static class Pair{
+        int first;
+        int second;
+        
+        public Pair(int first,int second){
+            this.first=first;
+            this.second=second;
+        }
+    }
+    public boolean helper(int src,ArrayList<ArrayList<Integer>> adj,boolean vis[]){
+        vis[src]=true;
+       Queue <Pair> q = new LinkedList<>();
+       q.add(new Pair(src,-1));
+         
+         while(!q.isEmpty()){
+             int node=q.peek().first;
+             int parent=q.peek().second;
+             q.remove();
+             
+             for(Integer i:adj.get(node)){
+                 if(vis[i]==false){
+                     vis[i]=true;
+                     q.add(new Pair(i,node));
+                 }
+                 else if(parent!=i){
+                     return true;
+                 }
+             }
+         }
+         return false;
+    }
+    
+    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        boolean vis[]=new boolean[V];
+        for(int i=0;i<V;i++){
+            vis[i]=false;
+        }
+        
+        for(int i=0;i<V;i++){
+            if(vis[i]==false){
+                if(helper(i,adj,vis)){
+                    return true;
+                };
+            }
+        }
+        return false;
+    }
